289c5a869c625cda3d0ee1689e1507478787509d
Tatiana Smolin <tatismolin@gmail.com>
2019-09-20 00:45:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,7 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-  source_array.map([]) {|n| n * 2}
+  source_array.map() {|n| n * 2}
 end
 
 def map_to_square(source_array)
