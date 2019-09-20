7237ceae96409176300e450099046ccee830981b
Tatiana Smolin <tatismolin@gmail.com>
2019-09-20 00:28:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,9 +20,9 @@ def reduce_to_total(source_array, starting_point)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.reduce {}
+  source_array.reduce(0) {|n| !n}
 end
 
 def reduce_to_any_true(source_array)
-  source_array.reduce {}
+  source_array.any? {|n| n}
 end
