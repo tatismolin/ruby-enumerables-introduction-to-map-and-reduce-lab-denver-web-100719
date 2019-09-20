ea209b115fcd129940c82bfe3aab5620fe57aa17
Tatiana Smolin <tatismolin@gmail.com>
2019-09-20 00:55:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,7 @@ def map_to_square(source_array)
   source_array.map {|n| n ** 2}
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array)
   source_array.reduce {|sum, n| sum + n}
 end
 
